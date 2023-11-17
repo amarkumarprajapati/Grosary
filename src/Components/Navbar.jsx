5976f594e275f0c5d3abf726e393d01b8081cce5
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2023-11-17 13:02:19 +0530

    main

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Cart from "../Storeinfo/Cart";
+
+const Navbar = () => {
+  // dropdown
+  const [dropshow, setDropshow] = useState(false);
+  const dropshownew = () => {
+    setDropshow(!dropshow);
+  };
+
+  const openset = () => {
+    window.open("/", "_self");
+  };
+
+  const [showdiv, setShowdiv] = useState(false);
+
+  const opencart = () => {
+    setShowdiv(!showdiv);
+  };
+
+  // call cart item
+  const [cartAll, setCartAll] = useState(false);
+
+  const toggleshow = () => {
+    setCartAll(!cartAll);
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="textalig">
+          <h1 className="h1tag" onClick={openset}>
+            ontheshelf
+          </h1>
+        </div>
+        <div className="searchbox">
+          <input type="text" placeholder="Search product" className="search" />
+        </div>
+        <img
+          src="123.png"
+          alt="img"
+          onClick={toggleshow}
+          className="image185"></img>
+        {cartAll && <Cart />}
+        <img src="12455.png" alt="img" className="image"></img>
+        <select name="user" id="">
+          <option value="">Profile</option>
+          <option value="">Setting</option>
+          <option value="">About</option>
+          <option value="">Contact US</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
