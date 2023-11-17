9647d01532b0fd9c37300b5f61e10efe1d6fbc25
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2023-11-17 12:28:19 +0530

    main

--- src/Pages/Storedetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Storedetails = () => {
-  return (
-    <div>Storedetails</div>
-  )
-}
-
-export default Storedetails
--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Global from './Global'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Global />
-  </React.StrictMode>,
-)
